df1c37993bb597734713d020e70e110c4611b2db
Harshith M <mharshith200@gmail.com>
2026-06-02 17:06:00 +0530

    Add: flood-fill - submission-0

---
+++ Data Structures & Algorithms/flood-fill/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+   public:
+   bool isValidCell(int currRow ,int currCol , int m , int n){
+    return currRow >= 0 && currRow < m && currCol >= 0 && currCol < n;
+   }
+    void dfs(int sr, int sc, vector<vector<int>>& ans, vector<vector<int>>& image, int newColor,
+             int delRow[], int delCol[], int iniColor) {
+        int m = image.size();
+        int n = image[0].size();
+        ans[sr][sc] = newColor;
+        for(int i=0; i<4; i++){
+            int nrow = sr + delRow[i];
+            int ncol = sc + delCol[i];
+            if(isValidCell(nrow , ncol , m , n) && image[nrow][ncol] == iniColor && ans[nrow][ncol] != newColor){
+                dfs(nrow , ncol , ans ,image , newColor , delRow , delCol , iniColor);
+            }
+
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        int m = image.size();
+        int n = image[0].size();
+        vector<vector<int>> ans = image;
+        int iniColor = image[sr][sc];
+        int delRow[] = {-1, 0, +1, 0};
+        int delCol[] = {0, +1, 0, -1};
+        dfs(sr, sc, ans, image, newColor, delRow, delCol, iniColor);
+        return ans;
+    }
+};
