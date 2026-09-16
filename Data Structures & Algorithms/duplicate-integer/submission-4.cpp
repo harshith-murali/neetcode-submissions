73e4da78f4f6153d4197e5be6951afc46834e9ed
Harshith M <mharshith200@gmail.com>
2026-09-16 11:37:42 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i-1] == nums[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+};
